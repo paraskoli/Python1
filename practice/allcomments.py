0a5744490caeea92077de23339009f03150cabcc
paraskoli <paraskoli43@gmail.com>
2022-06-10 23:55:54 +0530

    comments

---
+++ practice/allcomments.py
@@ -0,0 +1,7 @@
+#Hello Python  single line comments
+'Hello Python' #single line comments
+"Hello Python"   #single line comments
+'''Hello Python'''  # multiple line comments
+"""Hello Python"""   #multiple line comments
+a=45
+print(a)
---
+++ practice/string.py
@@ -0,0 +1,7 @@
+
+a= 'hello\n' #single_quoted string
+b="hello\n"  #double quoted string
+c='''hello\n'''  #triple quoted string
+d="""hello\n"""  #triple quoted string
+
+print(a,b,c,d)
