b6345cfcfc1ed04a865fc49ac5f65b8dd752cc01
paraskoli <paraskoli43@gmail.com>
2022-06-14 00:00:57 +0530

    fruits

---
+++ sets-methods.py
@@ -0,0 +1,17 @@
+#Creating an empty set
+# b=set()
+# print(type(b))
+
+# #adding value to the set
+# b.add(4)
+# b.add(5)
+# print(b)
+
+a={45,54,78,25}
+# a.add([4,5,5]) #we can not add list to set
+# print(type(a))
+a.add(545)
+print(a)
+
+a[0]=54
+print(a[0])
