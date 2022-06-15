4632f1b71689ceb4ae5094eefe444a4dd9f8c4f0
paraskoli <paraskoli43@gmail.com>
2022-06-15 22:30:26 +0530

    elif

---
+++ practice-sets/q1.py
@@ -0,0 +1,24 @@
+n1=int(input("enter the number :"))
+n2=int(input("enter the number :"))
+n3=int(input("enter the number :"))
+n4=int(input("enter the number :"))
+
+# if(n1>n2):
+#     print("n1 is a greatest number:",n1)
+# elif(n2>n3):
+#     print("n2 is a greatest number:",n2)
+# elif(n3>n4):
+#     print("n3 is a greatest number:",n3)
+# else:
+#     print("n4 is a greatest number:",n4)
+
+
+#or
+if(n1>n2):
+    print("n1 is greatest",n1)
+elif(n2>n3):
+    print("n2 is greatest",n2)
+elif(n3>n4):
+    print("n3 is greatest",n3)
+else:
+    print("n4 is greatest:",n4)
